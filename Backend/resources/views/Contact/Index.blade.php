2c9fde6ce1c6b0270757bd4e3477fac2dbca2f5c
khoidev203 <khoidev203@gmail.com>
2024-09-01 12:48:54 +0700

    update

--- Backend/resources/views/Contact/Index.blade.php
+++ Backend/resources/views/Contact/Index.blade.php
@@ -2,7 +2,7 @@
 @section('content_admin')
     <h1 class="text-center text-uppercase mt-3 mb-3">Danh Sách Liên Hệ</h1>
 
-    <a href="{{ route('contact.create') }}" class="btn btn-outline-success mb-3 px-4 fs-6">Thêm Mới</a>
+    <a href="{{ route('contact.create') }}" class="btn btn-outline-success mb-3 px-4 fs-6">Add</a>
     <table class="table table-bordered">
         <thead>
             <tr>
@@ -18,12 +18,12 @@
         <tbody>
             @foreach ($contacts as $contact)
                 <tr>
-                    <td>{{ $contact->id }}</td>
-                    <td>{{ $contact->fullname }}</td>
-                    <td>{{ $contact->email }}</td>
-                    <td>{{ $contact->title }}</td>
-                    <td>{{ $contact->message }}</td>
-                    <td>{{ $contact->phone }}</td>
+                    <td style="padding-top: 20px">{{ $contact->id }}</td>
+                    <td style="padding-top: 20px">{{ $contact->fullname }}</td>
+                    <td style="padding-top: 20px">{{ $contact->email }}</td>
+                    <td style="padding-top: 20px">{{ $contact->title }}</td>
+                    <td style="padding-top: 20px">{{ $contact->message }}</td>
+                    <td style="padding-top: 20px">{{ $contact->phone }}</td>
                     <td class="d-flex gap-3">
                         <form action="{{ route('contact.destroy', $contact->id) }}" method="POST">
                             @method('DELETE')
