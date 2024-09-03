4e3790becc536438fc1188f8e78e6d555c63b9d2
khoidev203 <khoidev203@gmail.com>
2024-09-03 18:50:37 +0700

    update

---
+++ frontend/src/Layout/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+import Form from "../../Component/ContactComponent/Form"
+import Info from "../../Component/ContactComponent/Info"
+import Map from "../../Component/ContactComponent/Map"
+
+const Contact:React.FC = () => {
+    return (
+        <>
+        <Map />
+        <Info />
+        <Form />
+        </>
+    )
+}
+
+export default Contact
